ce42c749f62e0d6d1e5cb24508e6deb0f91ed66d
Artur Pokrywka <pokrywkaart@gmail.com>
2019-05-08 19:52:08 +0200

    Add object Phone constructor

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('hi');
+'use strict';
+
+function Phone(brand, price, color, displaySize) {
+	this.brand = brand;
+	this.price = price;
+    this.color = color;
+    this.displaySize = displaySize;
+}
+
+Phone.prototype.printInfo = function() {
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "." + 
+    "It has " + this.displaySize + " inches display.");
+}
+
+var samsungGalaxyS6 = new Phone("Samsung", 1590, "black", 6.2);
+var iPhone6S = new Phone("Apple", 4250, "white", 5.8);
+var onePlusOne = new Phone("OnePlus", 2250, "silver", 7.1);
+
+samsungGalaxyS6.printInfo();
+iPhone6S.printInfo();
+onePlusOne.printInfo();
