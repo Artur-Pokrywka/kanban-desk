a8d18e11c7ba9948c6b97c8654eeef5ec4b1813a
Artur Pokrywka <pokrywkaart@gmail.com>
2019-05-09 11:36:26 +0200

    Did nothing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 'use strict';
-
 function Phone(brand, price, color, displaySize) {
 	this.brand = brand;
 	this.price = price;
     this.color = color;
     this.displaySize = displaySize;
+    // function getWarrantyCost() {
+    //     var self = this; 
+    //     return self.price * 0.1;
+    // }
+    // this.warrntyCost = getWarrantyCost();
+    // console.log(warrntyCost);
+
+    // this.warrntyCost = function() {
+    //     return self.price * 0.1;
+    // };
+    // console.log(this.warrntyCost);
 }
+
+Phone.prototype.getWarrantyCost = function() {
+    var self = this;
+    return self.price * 0.1;
+};
+
+samsungGalaxyS6.getWarrantyCost();
+
 
 Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "." + 
@@ -19,3 +37,25 @@ var onePlusOne = new Phone("OnePlus", 2250, "silver", 7.1);
 samsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
 onePlusOne.printInfo();
+
+// /////////////////////////////////////////////////////////////////////////
+
+function Button(text) {
+    this.text = text || "Hello";
+}
+
+Button.prototype = {
+	create: function() {
+        var self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function() {
+            alert(self.text);
+        });
+        document.body.appendChild(this.element);
+	}
+};
+
+var btn1 = new Button('Hello!');
+
+btn1.create();
