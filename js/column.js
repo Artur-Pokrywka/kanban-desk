1f26c8a7a386e3a7958a68a366fb34000db34d78
Artur Pokrywka <pokrywkaart@gmail.com>
2019-06-18 15:25:43 +0200

    Add Column function

--- js/column.js
+++ js/column.js
@@ -2,39 +2,47 @@ function Column(id, name) {
     let self = this;
     this.id = id;
     this.name = name || 'No name given';
+
     this.element = generateTemplate('column-template', { name: this.name, id: this.id });
 
-    if (event.target.classList.contains('add-card')) {
-        const cardName = prompt("Enter the name of the card");
-        event.preventDefault();
+    this.element.querySelector('.column').addEventListener('click', function (event){
+        if (event.target.classList.contains('add-card')) {
+            const cardName = prompt("Enter the name of the card");
+            event.preventDefault();
 
-        fetch(baseUrl + '/card', {
-            method: 'POST',
-            headers: myHeaders,
-            body: data,
-        })
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(function() {
-            let card = new Card(resp.id, cardName);
-            self.addCard(card);
-        });
-    }
-}
+            let data = new FormData();
+            data.append('name', cardName);
+            data.append('bootcamp_kanban_column_id', self.id);
+
+            fetch(baseUrl + '/card', {
+                method: 'POST',
+                headers: myHeaders,
+                body: data,
+            })
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(function(resp) {
+                let card = new Card(resp.id, cardName);
+                self.addCard(card);
+            });
+        }
+    })     
+};
 
 Column.prototype = {
   addCard: function(card) {
     this.element.querySelector('ul').appendChild(card.element);
   },
+  
   removeColumn: function() {
     let self = this;
     fetch(baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders })
         .then(function(resp) {
-        return resp.json();
+            return resp.json();
         })
-    .then(function(resp) {
-        self.element.parentNode.removeChild(self.element);
+        .then(function(resp) {
+            self.element.parentNode.removeChild(self.element);
     });
   }
 };
